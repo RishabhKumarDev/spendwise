f98d73f99928b38c3e3e2815a712733d65b4beb7
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2026-01-13 15:41:29 +0530

    fix(worker): reorder model imports for better organization

--- backend/src/worker.ts
+++ backend/src/worker.ts
@@ -2,6 +2,12 @@ import "dotenv/config";
 import connectDatabase from "./config/database.config";
 import { initlizeCrons } from "./crons";
 
+// model imports
+import "./models/user.model";
+import "./models/report-setting.model";
+import "./models/report.model";
+import "./models/transaction.model";
+
 const init = async () => {
   await connectDatabase();
   await initlizeCrons();
