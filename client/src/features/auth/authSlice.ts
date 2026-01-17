9fa5d15bc5abd0e502c06f501ba1e89266726d8e
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2026-01-17 19:28:02 +0530

    feat(auth): add auth slice & auth endpoints

---
+++ client/src/features/auth/authSlice.ts
@@ -0,0 +1,75 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  id: number;
+  email: string;
+  name: string;
+  profilePicture: string;
+}
+
+interface ReportSetting {
+  userId: string;
+  frequency?: string;
+  isEnabled: boolean;
+}
+
+interface AuthState {
+  accessToken: string | null;
+  expiresAt: number | null;
+  user: User | null;
+  reportSetting: ReportSetting | null;
+}
+
+const initialState: AuthState = {
+  accessToken: null,
+  expiresAt: null,
+  user: null,
+  reportSetting: null,
+};
+
+type PayloadActionType = PayloadAction<{
+  token: string | null;
+  expiresAt: number | null;
+  user: User | null;
+  reportSetting: ReportSetting | null;
+}>;
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action: PayloadActionType) => {
+      console.log("AUTH SLICE", action.payload);
+      state.accessToken = action.payload.token;
+      state.expiresAt = action.payload.expiresAt;
+      state.user = action.payload.user;
+      state.reportSetting = action.payload.reportSetting;
+    },
+    updateCredentials: (state, action: PayloadActionType) => {
+      console.log("AUTH SLICE", action.payload);
+
+      const { token, expiresAt, user, reportSetting } = action.payload;
+
+      if (token !== undefined) state.accessToken = token;
+      if (expiresAt !== undefined) state.expiresAt = expiresAt;
+      if (user !== undefined) {
+        state.user = state.user ? { ...state.user, ...user } : user;
+      }
+      
+      if (reportSetting !== undefined) {
+        state.reportSetting = state.reportSetting
+          ? { ...state.reportSetting, ...reportSetting }
+          : reportSetting;
+      }
+    },
+    logout: (state) => {
+      state.accessToken = null;
+      state.expiresAt = null;
+      state.user = null;
+      state.reportSetting = null;
+    },
+  },
+});
+
+export const { setCredentials, updateCredentials, logout } = authSlice.actions;
+export default authSlice.reducer;
