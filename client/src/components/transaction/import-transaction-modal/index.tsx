9267cc9266605b28c1dfb097f182b5269397c552
Rishabh <rishabhkumardev01@gmail.com>
2026-02-17 22:33:27 +0530

    feat(bulk Import): add bulk Import component & file upload step

---
+++ client/src/components/transaction/import-transaction-modal/index.tsx
@@ -0,0 +1,100 @@
+import { CsvColumn, TransactionField } from "@/@types/transaction.type";
+import ColumnMappingStep from "@/components/transaction/import-transaction-modal/column-mapping-step";
+import ConfirmationStep from "@/components/transaction/import-transaction-modal/confirmation-step";
+import FileUploadStep from "@/components/transaction/import-transaction-modal/fileupload-step";
+import { Button } from "@/components/ui/button";
+import { DialogContent } from "@/components/ui/dialog";
+import { Dialog } from "@radix-ui/react-dialog";
+import { ImportIcon } from "lucide-react";
+import { useState } from "react";
+
+function ImportTransactionModal() {
+  const [step, setStep] = useState<1 | 2 | 3>(1);
+  const [file, setFile] = useState<File | null>(null);
+  const [csvColumns, setCsvColumns] = useState<CsvColumn[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [csvData, setCsvData] = useState<any[]>([]);
+  const [mappings, setMappings] = useState<Record<string, string>>({});
+  const [open, setOpen] = useState(false);
+
+  const transactionFields: TransactionField[] = [
+    { fieldName: "title", required: true },
+    { fieldName: "amount", required: true },
+    { fieldName: "type", required: true },
+    { fieldName: "date", required: true },
+    { fieldName: "category", required: true },
+    { fieldName: "paymentMethod", required: true },
+    { fieldName: "description", required: false },
+  ];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleFileUpload = (file: File, columns: CsvColumn[], data: any[]) => {
+    setFile(file);
+    setCsvColumns(columns);
+    setCsvData(data);
+    setMappings({});
+    setStep(2);
+  };
+
+  const resetImport = () => {
+    setFile(null);
+    setCsvColumns([]);
+    setMappings({});
+    setStep(1);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setTimeout(() => {
+      resetImport();
+    }, 300);
+  };
+
+  const handleMappingcomplete = (mappings: Record<string, string>) => {
+    setMappings(mappings);
+    setStep(3);
+  };
+
+  const handleBack = (step: 1 | 2 | 3) => {
+    setStep(step);
+  };
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <FileUploadStep onFileUpload={handleFileUpload} />;
+      case 2:
+        return (
+          <ColumnMappingStep
+            onBack={() => handleBack(1)}
+            onComplete={handleMappingcomplete}
+            csvColumns={csvColumns}
+            mappings={mappings}
+            transactionFields={transactionFields}
+          />
+        );
+      case 3:
+        return <ConfirmationStep />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <Button
+        variant="outline"
+        className="!shadow-none !cursor-pointer !border-gray-500 !text-white !bg-transparent"
+        onClick={() => setOpen(true)}
+      >
+        <ImportIcon className="!w-5 !h-5" />
+        Bulk Import
+      </Button>
+      <DialogContent className=" max-w-2xl min-h-[40vh]">
+        {renderStep()}
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ImportTransactionModal;
