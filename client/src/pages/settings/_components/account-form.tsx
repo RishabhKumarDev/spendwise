6cdd9af780ad4c0a4a9bbb218852c80b80bf8817
Rishabh <rishabhkumardev01@gmail.com>
2026-02-28 22:18:10 +0530

    fix outDir, env, validation.

--- client/src/pages/settings/_components/account-form.tsx
+++ client/src/pages/settings/_components/account-form.tsx
@@ -22,7 +22,7 @@ import { z } from "zod";
 const accountFormSchema = z.object({
   name: z
     .string()
-    .min(2, {
+    .min(3, {
       message: "Name must be at least 2 characters",
     })
     .optional(),
